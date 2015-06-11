54a77d2dcd08c2e9a2aa3cc81bdceb88959f24cb
BurnUalive <burnualive@hotmail.com>
2015-06-11 18:59:50 +0530

    Route test and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # India Emerge Youth Summit
 Website for India Emerge Youth Summit
+
+>Run _bower install_ in terminal to install dependencies
+>>Find out how to install bower [here](bower.io).
--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
-/**
- * Created by Shivam Mathur on 11-06-2015.
- */
-angular.module('ieys', ['ngMaterial'])
-    .controller('ctrl', function($scope) {
-    })
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('black')
-            .accentPalette('red')});
+var app = angular.module('IEYS', ['ngMaterial','ngRoute']);
+
+var controllers ={};
+controllers.header = function($scope){$scope.name = 'India Emerge Youth Summit';};
+controllers.content = function($scope){$scope.content = 'Controller Change';};
+app.controller(controllers);
+
+app.config(function($mdThemingProvider,$routeProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('grey',{
+            'default':'700'
+        })
+        .accentPalette('red',{
+            'default': 'A700'
+        });
+    $routeProvider
+        .when('/',{
+            templateUrl: 'views/main.html'
+        })
+        .when('/route1',{
+            templateUrl: 'views/route1.html',
+            controller:'content'
+        })
+        .otherwise({
+            redirectTo : '/'
+        });
+});
+
 
