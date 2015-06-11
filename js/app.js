2c44a08b04397eefda9d487dc12d05263edcb850
BurnUalive <burnualive@hotmail.com>
2015-06-11 14:02:16 +0530

    app setup and test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /**
  * Created by Shivam Mathur on 11-06-2015.
  */
+angular.module('ieys', ['ngMaterial'])
+    .controller('ctrl', function($scope) {
+    })
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('black')
+            .accentPalette('red')});
+
